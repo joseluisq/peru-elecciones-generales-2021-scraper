89e172e9cedcc51814d1644717e70613d0ebe50f
Jose Quintana <joseluisquintana20@gmail.com>
2021-04-15 17:19:22 +0200

    refactor: simplify json data object props

--- procesador.dart
+++ procesador.dart
@@ -16,11 +16,11 @@ class JsonResults {
         summary = Summary.fromJson(json['summary'] as Map<String, dynamic>);
 
   Map<String, dynamic> toJson() {
-    final data = <String, dynamic>{};
-    data['generals'] = generals.toJson();
-    data['results'] = results.map((v) => v.toJson()).toList();
-    data['summary'] = summary.toJson();
-    return data;
+    return {
+      'generals': generals.toJson(),
+      'results': results.map((v) => v.toJson()).toList(),
+      'summary': summary.toJson(),
+    };
   }
 }
 
@@ -36,205 +36,204 @@ class Generals {
         realImages = json['realImages'] as String;
 
   Map<String, dynamic> toJson() {
-    final data = <String, dynamic>{};
-    data['generalData'] = generalData.toJson();
-    data['actData'] = actData.toJson();
-    data['realImages'] = realImages;
-    return data;
+    return {
+      'generalData': generalData.toJson(),
+      'actData': actData.toJson(),
+      'realImages': realImages,
+    };
   }
 }
 
 class GeneralData {
-  final String eLECTORESHABIL;
-  final String tOTCIUDADANOSVOTARON;
-  final String pORCIUDADANOSVOTARON;
-  final String pORACTASCONTABILIZADAS;
-  final String pORACTASPROCESADAS;
-  final String aCTASPROCESADAS;
-  final String pORPROCESAR;
-  final String cONTABILIZA;
-  final String pORPORPROCESAR;
-  final String nCANDIDATOS;
+  final String electoresHabiles;
+  final String totalCiudadanosVotaron;
+  final String porCiudadanosVotaron;
+  final String porActasContabilizadas;
+  final String porActasProcesadas;
+  final String actasProcesadas;
+  final String porProcesar;
+  final String contabiliza;
+  final String porPorProcesar;
+  final String nCandidatos;
 
   GeneralData.fromJson(Map<String, dynamic> json)
-      : eLECTORESHABIL = json['ELECTORES_HABIL'] as String,
-        tOTCIUDADANOSVOTARON = json['TOT_CIUDADANOS_VOTARON'] as String,
-        pORCIUDADANOSVOTARON = json['POR_CIUDADANOS_VOTARON'] as String,
-        pORACTASCONTABILIZADAS = json['POR_ACTAS_CONTABILIZADAS'] as String,
-        pORACTASPROCESADAS = json['POR_ACTAS_PROCESADAS'] as String,
-        aCTASPROCESADAS = json['ACTAS_PROCESADAS'] as String,
-        pORPROCESAR = json['POR_PROCESAR'] as String,
-        cONTABILIZA = json['CONTABILIZA'] as String,
-        pORPORPROCESAR = json['POR_POR_PROCESAR'] as String,
-        nCANDIDATOS = json['N_CANDIDATOS'] as String;
+      : electoresHabiles = json['ELECTORES_HABIL'] as String,
+        totalCiudadanosVotaron = json['TOT_CIUDADANOS_VOTARON'] as String,
+        porCiudadanosVotaron = json['POR_CIUDADANOS_VOTARON'] as String,
+        porActasContabilizadas = json['POR_ACTAS_CONTABILIZADAS'] as String,
+        porActasProcesadas = json['POR_ACTAS_PROCESADAS'] as String,
+        actasProcesadas = json['ACTAS_PROCESADAS'] as String,
+        porProcesar = json['POR_PROCESAR'] as String,
+        contabiliza = json['CONTABILIZA'] as String,
+        porPorProcesar = json['POR_POR_PROCESAR'] as String,
+        nCandidatos = json['N_CANDIDATOS'] as String;
 
-  Map<String, dynamic> toJson() {
-    final data = <String, dynamic>{};
-    data['ELECTORES_HABIL'] = eLECTORESHABIL;
-    data['TOT_CIUDADANOS_VOTARON'] = tOTCIUDADANOSVOTARON;
-    data['POR_CIUDADANOS_VOTARON'] = pORCIUDADANOSVOTARON;
-    data['POR_ACTAS_CONTABILIZADAS'] = pORACTASCONTABILIZADAS;
-    data['POR_ACTAS_PROCESADAS'] = pORACTASPROCESADAS;
-    data['ACTAS_PROCESADAS'] = aCTASPROCESADAS;
-    data['POR_PROCESAR'] = pORPROCESAR;
-    data['CONTABILIZA'] = cONTABILIZA;
-    data['POR_POR_PROCESAR'] = pORPORPROCESAR;
-    data['N_CANDIDATOS'] = nCANDIDATOS;
-    return data;
+  Map<String, String> toJson() {
+    return {
+      'ELECTORES_HABIL': electoresHabiles,
+      'TOT_CIUDADANOS_VOTARON': totalCiudadanosVotaron,
+      'POR_CIUDADANOS_VOTARON': porCiudadanosVotaron,
+      'POR_ACTAS_CONTABILIZADAS': porActasContabilizadas,
+      'POR_ACTAS_PROCESADAS': porActasProcesadas,
+      'ACTAS_PROCESADAS': actasProcesadas,
+      'POR_PROCESAR': porProcesar,
+      'CONTABILIZA': contabiliza,
+      'POR_POR_PROCESAR': porPorProcesar,
+      'N_CANDIDATOS': nCandidatos,
+    };
   }
 }
 
 class ActData {
-  final String fECHA;
-  final String hORA;
-  final String pORAVANCE;
+  final String fecha;
+  final String hora;
+  final String porAvance;
 
   ActData.fromJson(Map<String, dynamic> json)
-      : fECHA = json['FECHA'] as String,
-        hORA = json['HORA'] as String,
-        pORAVANCE = json['POR_AVANCE'] as String;
+      : fecha = json['FECHA'] as String,
+        hora = json['HORA'] as String,
+        porAvance = json['POR_AVANCE'] as String;
 
-  Map<String, dynamic> toJson() {
-    final data = <String, dynamic>{};
-    data['FECHA'] = fECHA;
-    data['HORA'] = hORA;
-    data['POR_AVANCE'] = pORAVANCE;
-    return data;
+  Map<String, String> toJson() {
+    return {
+      'FECHA': fecha,
+      'HORA': hora,
+      'POR_AVANCE': porAvance,
+    };
   }
 }
 
 class Results {
-  final String? cCODIAGRUPOL;
-  final String aGRUPACION;
-  final String tOTALVOTOS;
-  final String? nLISTA;
-  final String? pORVALIDOS;
-  final String pOREMITIDOS;
-  final String? nOMBREeCANDIDATO;
-  final String? iMAGENAGRUPACION;
-  final String? pORCAVANCEACTA;
-  final String? aCTASCOMPUTADA;
-  final String? tOTALMESAS;
+  final String? cCodiAgruPol;
+  final String agrupacion;
+  final String totalVotos;
+  final String? nLista;
+  final String? porValidos;
+  final String porEmitidos;
+  final String? nombreCandidato;
+  final String? imagenAgrupacion;
+  final String? porcAvanceActa;
+  final String? actasComputadas;
+  final String? totalMesas;
 
   Results.fromJson(Map<String, dynamic> json)
-      : cCODIAGRUPOL = json['C_CODI_AGRUPOL'] as String?,
-        aGRUPACION = json['AGRUPACION'] as String,
-        tOTALVOTOS = json['TOTAL_VOTOS'] as String,
-        nLISTA = json['NLISTA'] as String?,
-        pORVALIDOS = json['POR_VALIDOS'] as String?,
-        pOREMITIDOS = json['POR_EMITIDOS'] as String,
-        nOMBREeCANDIDATO = json['NOMBREe_CANDIDATO'] as String?,
-        iMAGENAGRUPACION = json['IMAGENAGRUPACION'] as String?,
-        pORCAVANCEACTA = json['PORC_AVANCE_ACTA'] as String?,
-        aCTASCOMPUTADA = json['ACTAS_COMPUTADA'] as String?,
-        tOTALMESAS = json['TOTAL_MESAS'] as String?;
+      : cCodiAgruPol = json['C_CODI_AGRUPOL'] as String?,
+        agrupacion = json['AGRUPACION'] as String,
+        totalVotos = json['TOTAL_VOTOS'] as String,
+        nLista = json['NLISTA'] as String?,
+        porValidos = json['POR_VALIDOS'] as String?,
+        porEmitidos = json['POR_EMITIDOS'] as String,
+        nombreCandidato = json['NOMBREe_CANDIDATO'] as String?,
+        imagenAgrupacion = json['IMAGENAGRUPACION'] as String?,
+        porcAvanceActa = json['PORC_AVANCE_ACTA'] as String?,
+        actasComputadas = json['ACTAS_COMPUTADA'] as String?,
+        totalMesas = json['TOTAL_MESAS'] as String?;
 
   Map<String, dynamic> toJson() {
-    final data = <String, dynamic>{};
-    data['C_CODI_AGRUPOL'] = cCODIAGRUPOL;
-    data['AGRUPACION'] = aGRUPACION;
-    data['TOTAL_VOTOS'] = tOTALVOTOS;
-    data['NLISTA'] = nLISTA;
-    data['POR_VALIDOS'] = pORVALIDOS;
-    data['POR_EMITIDOS'] = pOREMITIDOS;
-    data['NOMBREe_CANDIDATO'] = nOMBREeCANDIDATO;
-    data['IMAGENAGRUPACION'] = iMAGENAGRUPACION;
-    data['PORC_AVANCE_ACTA'] = pORCAVANCEACTA;
-    data['ACTAS_COMPUTADA'] = aCTASCOMPUTADA;
-    data['TOTAL_MESAS'] = tOTALMESAS;
-    return data;
+    return {
+      'C_CODI_AGRUPOL': cCodiAgruPol,
+      'AGRUPACION': agrupacion,
+      'TOTAL_VOTOS': totalVotos,
+      'NLISTA': nLista,
+      'POR_VALIDOS': porValidos,
+      'POR_EMITIDOS': porEmitidos,
+      'NOMBREe_CANDIDATO': nombreCandidato,
+      'IMAGENAGRUPACION': imagenAgrupacion,
+      'PORC_AVANCE_ACTA': porcAvanceActa,
+      'ACTAS_COMPUTADA': actasComputadas,
+      'TOTAL_MESAS': totalMesas,
+    };
   }
 }
 
 class Summary {
-  final String cCODIUBIGEO;
-  final String cONTABILIZA;
-  final String cONTNORMAL;
-  final String iMPUGNADA;
-  final String eRRORMAT;
-  final String mESASINSTALADAS;
-  final String aCTASPROCESADAS;
-  final String sOLNULIDAD;
-  final String iLEGIBLE;
-  final String pENDIENTE;
-  final String oTRASOBS;
-  final String sINDATOS;
-  final String cONTANULADA;
-  final String pORPROCESAR;
-  final String eXTRAVIADA;
-  final String sINIESTRADA;
-  final String iNCOMPLETA;
-  final String sINFIRMA;
-  final String aINSTALAR;
-  final String mESASNOINST;
-  final String fUSIONADAS;
-  final String nTOTALAUTO;
-  final String mESASHABILES;
-  final String tOTCIUDADANOSVOTARON;
-  final String eLECTORESHABIL;
-  final String eLECTORESHABIL50;
-  final String tOTALJEE;
+  final String cCodiUbigeo;
+  final String contabiliza;
+  final String contNormal;
+  final String impugnada;
+  final String errorMat;
+  final String mesasInstaladas;
+  final String actasProcesadas;
+  final String solNulidad;
+  final String ilegible;
+  final String pendiente;
+  final String otrasObs;
+  final String sinDatos;
+  final String contAnulada;
+  final String porProcesar;
+  final String extraviada;
+  final String siniestrada;
+  final String incompleta;
+  final String sinFirma;
+  final String aIinstalar;
+  final String mesasNoInst;
+  final String fusionadas;
+  final String nTotalAuto;
+  final String mesasHabiles;
+  final String totCiudadanosVotaron;
+  final String electoresHabil;
+  final String electoresHabil50;
+  final String totalJEE;
 
   Summary.fromJson(Map<String, dynamic> json)
-      : cCODIUBIGEO = json['CCODI_UBIGEO'] as String,
-        cONTABILIZA = json['CONTABILIZA'] as String,
-        cONTNORMAL = json['CONT_NORMAL'] as String,
-        iMPUGNADA = json['IMPUGNADA'] as String,
-        eRRORMAT = json['ERROR_MAT'] as String,
-        mESASINSTALADAS = json['MESAS_INSTALADAS'] as String,
-        aCTASPROCESADAS = json['ACTAS_PROCESADAS'] as String,
-        sOLNULIDAD = json['SOL_NULIDAD'] as String,
-        iLEGIBLE = json['ILEGIBLE'] as String,
-        pENDIENTE = json['PENDIENTE'] as String,
-        oTRASOBS = json['OTRAS_OBS'] as String,
-        sINDATOS = json['SIN_DATOS'] as String,
-        cONTANULADA = json['CONT_ANULADA'] as String,
-        pORPROCESAR = json['POR_PROCESAR'] as String,
-        eXTRAVIADA = json['EXTRAVIADA'] as String,
-        sINIESTRADA = json['SINIESTRADA'] as String,
-        iNCOMPLETA = json['INCOMPLETA'] as String,
-        sINFIRMA = json['SIN_FIRMA'] as String,
-        aINSTALAR = json['A_INSTALAR'] as String,
-        mESASNOINST = json['MESAS_NO_INST'] as String,
-        fUSIONADAS = json['FUSIONADAS'] as String,
-        nTOTALAUTO = json['NTOTAL_AUTO'] as String,
-        mESASHABILES = json['MESAS_HABILES'] as String,
-        tOTCIUDADANOSVOTARON = json['TOT_CIUDADANOS_VOTARON'] as String,
-        eLECTORESHABIL = json['ELECTORES_HABIL'] as String,
-        eLECTORESHABIL50 = json['ELECTORES_HABIL_50'] as String,
-        tOTALJEE = json['TOTAL_JEE'] as String;
+      : cCodiUbigeo = json['CCODI_UBIGEO'] as String,
+        contabiliza = json['CONTABILIZA'] as String,
+        contNormal = json['CONT_NORMAL'] as String,
+        impugnada = json['IMPUGNADA'] as String,
+        errorMat = json['ERROR_MAT'] as String,
+        mesasInstaladas = json['MESAS_INSTALADAS'] as String,
+        actasProcesadas = json['ACTAS_PROCESADAS'] as String,
+        solNulidad = json['SOL_NULIDAD'] as String,
+        ilegible = json['ILEGIBLE'] as String,
+        pendiente = json['PENDIENTE'] as String,
+        otrasObs = json['OTRAS_OBS'] as String,
+        sinDatos = json['SIN_DATOS'] as String,
+        contAnulada = json['CONT_ANULADA'] as String,
+        porProcesar = json['POR_PROCESAR'] as String,
+        extraviada = json['EXTRAVIADA'] as String,
+        siniestrada = json['SINIESTRADA'] as String,
+        incompleta = json['INCOMPLETA'] as String,
+        sinFirma = json['SIN_FIRMA'] as String,
+        aIinstalar = json['A_INSTALAR'] as String,
+        mesasNoInst = json['MESAS_NO_INST'] as String,
+        fusionadas = json['FUSIONADAS'] as String,
+        nTotalAuto = json['NTOTAL_AUTO'] as String,
+        mesasHabiles = json['MESAS_HABILES'] as String,
+        totCiudadanosVotaron = json['TOT_CIUDADANOS_VOTARON'] as String,
+        electoresHabil = json['ELECTORES_HABIL'] as String,
+        electoresHabil50 = json['ELECTORES_HABIL_50'] as String,
+        totalJEE = json['TOTAL_JEE'] as String;
 
   Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    final data = map;
-    data['CCODI_UBIGEO'] = cCODIUBIGEO;
-    data['CONTABILIZA'] = cONTABILIZA;
-    data['CONT_NORMAL'] = cONTNORMAL;
-    data['IMPUGNADA'] = iMPUGNADA;
-    data['ERROR_MAT'] = eRRORMAT;
-    data['MESAS_INSTALADAS'] = mESASINSTALADAS;
-    data['ACTAS_PROCESADAS'] = aCTASPROCESADAS;
-    data['SOL_NULIDAD'] = sOLNULIDAD;
-    data['ILEGIBLE'] = iLEGIBLE;
-    data['PENDIENTE'] = pENDIENTE;
-    data['OTRAS_OBS'] = oTRASOBS;
-    data['SIN_DATOS'] = sINDATOS;
-    data['CONT_ANULADA'] = cONTANULADA;
-    data['POR_PROCESAR'] = pORPROCESAR;
-    data['EXTRAVIADA'] = eXTRAVIADA;
-    data['SINIESTRADA'] = sINIESTRADA;
-    data['INCOMPLETA'] = iNCOMPLETA;
-    data['SIN_FIRMA'] = sINFIRMA;
-    data['A_INSTALAR'] = aINSTALAR;
-    data['MESAS_NO_INST'] = mESASNOINST;
-    data['FUSIONADAS'] = fUSIONADAS;
-    data['NTOTAL_AUTO'] = nTOTALAUTO;
-    data['MESAS_HABILES'] = mESASHABILES;
-    data['TOT_CIUDADANOS_VOTARON'] = tOTCIUDADANOSVOTARON;
-    data['ELECTORES_HABIL'] = eLECTORESHABIL;
-    data['ELECTORES_HABIL_50'] = eLECTORESHABIL50;
-    data['TOTAL_JEE'] = tOTALJEE;
-    return data;
+    return {
+      'CCODI_UBIGEO': cCodiUbigeo,
+      'CONTABILIZA': contabiliza,
+      'CONT_NORMAL': contNormal,
+      'IMPUGNADA': impugnada,
+      'ERROR_MAT': errorMat,
+      'MESAS_INSTALADAS': mesasInstaladas,
+      'ACTAS_PROCESADAS': actasProcesadas,
+      'SOL_NULIDAD': solNulidad,
+      'ILEGIBLE': ilegible,
+      'PENDIENTE': pendiente,
+      'OTRAS_OBS': otrasObs,
+      'SIN_DATOS': sinDatos,
+      'CONT_ANULADA': contAnulada,
+      'POR_PROCESAR': porProcesar,
+      'EXTRAVIADA': extraviada,
+      'SINIESTRADA': siniestrada,
+      'INCOMPLETA': incompleta,
+      'SIN_FIRMA': sinFirma,
+      'A_INSTALAR': aIinstalar,
+      'MESAS_NO_INST': mesasNoInst,
+      'FUSIONADAS': fusionadas,
+      'NTOTAL_AUTO': nTotalAuto,
+      'MESAS_HABILES': mesasHabiles,
+      'TOT_CIUDADANOS_VOTARON': totCiudadanosVotaron,
+      'ELECTORES_HABIL': electoresHabil,
+      'ELECTORES_HABIL_50': electoresHabil50,
+      'TOTAL_JEE': totalJEE,
+    };
   }
 }
 
@@ -247,8 +246,8 @@ Future<void> main() async {
   // TODO: print HTML page using this JSON data
   print('Elecciones Generales 2021');
   print('-------------------------');
-  print('Fecha: ${obj.generals.actData.fECHA}');
-  print('Hora: ${obj.generals.actData.hORA}');
-  print('Por Avance: ${obj.generals.actData.pORAVANCE.trim()}');
-  print('Por Avance: ${obj.results[0].tOTALVOTOS}');
+  print('Fecha: ${obj.generals.actData.fecha}');
+  print('Hora: ${obj.generals.actData.hora}');
+  print('Por Avance: ${obj.generals.actData.porAvance.trim()}');
+  print('Por Avance: ${obj.results[0].totalVotos}');
 }
